8cae0c199bc770c2799125a3f34f18b311004bbc
Tu Tran <129575993+hyji916@users.noreply.github.com>
2025-10-10 23:00:56 -0500

    spanihs to eng for now

---
+++ hustlersociety/src/app/spanish/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HeroEnglish from "../components/HeroEnglish"; // Adjust relative to the current file
+import ApplySection from "../components/ApplySection";
+import InfoSection from "../components/InfoSection";
+import TestimonialSection from "../components/TestimonalSection";
+
+export default function EnglishPage(): JSX.Element {
+  return (
+    <main>
+      <HeroEnglish />
+      {/* Add more sections here as you build out your English content */}
+      <ApplySection />
+      <InfoSection />
+      <TestimonialSection />
+    </main>
+  );
+}
