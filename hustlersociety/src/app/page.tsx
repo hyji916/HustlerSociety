1116c7e69ec1b0fcd552dcc8a26c452a65f1838f
hyji916 <jaketran231@gmail.com>
2025-10-17 08:51:32 -0500

    eslint

--- hustlersociety/src/app/page.tsx
+++ hustlersociety/src/app/page.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import Footer from "./components/Footer";
 import HeroFirst from "./components/HeroFirst";
 
-type Language = "spanish" | "english";
-
 export default function LandingPage() {
   return (
     <main className="bg-black">
