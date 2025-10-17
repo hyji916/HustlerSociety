db5c4c8e22106075c1d56bcd7feb5dc537c8db0c
hyji916 <jaketran231@gmail.com>
2025-10-17 08:49:18 -0500

    removed imports for unused

--- hustlersociety/src/app/english/page.tsx
+++ hustlersociety/src/app/english/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import HeroEnglish from "../components/HeroEnglish"; // Adjust relative to the current file
-import ApplySection from "../components/ApplySection";
 import InfoSection from "../components/InfoSection";
 import TestimonialSection from "../components/TestimonalSection";
 import GallerySection from "../components/GallerySectionEnglish";
--- hustlersociety/src/app/page.tsx
+++ hustlersociety/src/app/page.tsx
@@ -1,35 +1,15 @@
 "use client";
 
 import React from "react";
-import HeroEnglish from "./components/HeroEnglish"; // Adjust relative to the current file
-import ApplySection from "./components/ApplySection";
-import InfoSection from "./components/InfoSection";
-import TestimonialSection from "./components/TestimonalSection";
-import GallerySection from "./components/GallerySectionEnglish";
-import CtaButtonComponent from "./components/CtaButtonComponent";
 import Footer from "./components/Footer";
-import GHLPopupForm from "./components/GHLPopupForm";
 import HeroFirst from "./components/HeroFirst";
 
 type Language = "spanish" | "english";
 
 export default function LandingPage() {
-  const handleLanguageSelect = (language: Language): void => {
-    window.location.href = `/${language}`;
-  };
-
   return (
     <main className="bg-black">
       <HeroFirst />
-      {/* Add more sections here as you build out your English content */}
-      {/* <ApplySection /> */}
-      {/* <GHLPopupForm /> */}
-      {/* <InfoSection />
-      <CtaButtonComponent />
-      <TestimonialSection />
-      <CtaButtonComponent />
-      <GallerySection />
-      <CtaButtonComponent />  */}
       <Footer />
     </main>
   );
